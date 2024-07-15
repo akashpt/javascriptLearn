ce17d0a1ccf5b1ba3a29e4792084b5b62cdb282e
Akash Pralhad Thakre <56046174+akashpt@users.noreply.github.com>
2024-07-15 19:43:56 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score = "true"
+//console.log(typeof score); //number
+//console.log(typeof (score)); //number
+
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ac" => nan
+// true =>; false=>0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitsh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//############### Operations ###################//
+
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**2); // 4
+console.log(2/3); // 0.6666666
+console.log(2%3); // 2
+
+let str1 = "Hello"
+let str2 = " Akash"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
